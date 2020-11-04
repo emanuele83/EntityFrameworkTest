31f2718851964748ea5b5ff81d538a0cae937592
emanuele <emanuele.sabbadini@gmail.com>
2020-11-04 20:15:17 +0100

    section 1 - DB first

---
+++ EntityFrameworkDatabaseFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDatabaseFirst
+{
+	class Program
+	{
+		public enum LevelEnum: byte
+		{
+			Beginner = 1,
+			Intermediate = 2,
+			Advanced = 3
+		}
+
+		static void Main(string[] args)
+		{
+			var dbContext = new PlutoDbContext();
+			var courses = dbContext.GetCourses();
+
+			foreach (var c in courses)
+			{
+				Console.WriteLine(c.Title);
+			}
+
+			var course = new Course();
+			course.Level = LevelEnum.Advanced;
+
+			Console.ReadLine();
+		}
+	}
+}
