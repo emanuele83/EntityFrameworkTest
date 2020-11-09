2c9b7652b40e588477ba72063ab8418328e5d24b
emanuele <emanuele.sabbadini@gmail.com>
2020-11-09 12:38:39 +0100

    tets code first

---
+++ VidzyCodeFirst/VidzyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidzyCodeFirst.Models;
+
+namespace VidzyCodeFirst
+{
+	public class VidzyDbContext : DbContext
+	{
+		public DbSet<Video> Videos { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+	}
+}
